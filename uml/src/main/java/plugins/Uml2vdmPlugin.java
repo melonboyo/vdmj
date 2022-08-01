dc610012fbfec3f6ab95de860446982e748d7b2d
Lucas Bjarke Jensen <lucas97jensen@gmail.com>
2022-08-01 15:49:13 +0200

    reading class names from xmi

--- uml/src/main/java/plugins/Uml2vdmPlugin.java
+++ uml/src/main/java/plugins/Uml2vdmPlugin.java
@@ -2,13 +2,22 @@ package plugins;
 
 import java.io.File;
 import com.fujitsu.vdmj.commands.CommandPlugin;
-import com.fujitsu.vdmj.runtime.ClassInterpreter;
 import com.fujitsu.vdmj.runtime.Interpreter;
+
+import com.fujitsu.vdmj.runtime.ClassInterpreter;
 import com.fujitsu.vdmj.tc.definitions.TCClassDefinition;
 import com.fujitsu.vdmj.tc.definitions.TCClassList; 
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 public class Uml2vdmPlugin extends CommandPlugin {
     
@@ -32,14 +41,33 @@ public class Uml2vdmPlugin extends CommandPlugin {
 			return true;
 		}	
 
-		File file = new File(path);
+		try 
+		{
+			File inputFile = new File(path);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+         	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+         	Document doc = dBuilder.parse(inputFile);
+         	doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("UML:Class");
+			System.out.println("----------------------------");
 
-		BufferedReader br = new BufferedReader(new FileReader(file));
- 		String line;
- 		while ((line = br.readLine()) != null) {
-   			System.out.println(line);
- 		} br.close();
-		
+			for (int temp = 0; temp < nList.getLength(); temp++) 
+			{
+				Node nNode = nList.item(temp);
+				
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					System.out.println("Class : " 
+					   + eElement.getAttribute("name"));
+
+				
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return true;
 	}
 	
